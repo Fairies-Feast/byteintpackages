deb1b69942877278717dc3f75f938b48c7943514
Fairies0feast on Replit <86023563+Fairies-Feast@users.noreply.github.com>
2023-10-18 16:15:01 -0500

    Create __init__.py

---
+++ colorama/__init__.py
@@ -0,0 +1,17 @@
+from __byteint import raw
+class Fore:
+  RESET = raw("</span>")
+  BLACK = raw("<span style='color:black;'>")
+  BLUE = raw("<span style='color:blue;'>")
+  CYAN = raw("<span style='color:cyan;'")
+  GREEN = raw("<span style='color:green;'")
+  LIGHTBLACK_EX = raw("<span style='color:grey;'>")
+  LIGHTBLUE_EX = raw("<span style='color:skyblue;'>")
+  LIGHTCYAN_EX = raw("<span style='color:#E0FFFF'>")
+  LIGHTGREEN_EX = raw("<span style='color:lightgreen;'>")
+  LIGHTMAGENTA_EX = raw("<span style='color:#FF00FF;'>")
+  LIGHTRED_EX = raw("<span style='color:#ffcccb'>")
+  LIGHTYELLOW_EX = raw("<span style='color:yellow;'>")
+  MAGENTA = raw("<span style='color:#FF00FF;'>")
+  RED = raw("<span style='color:red;'>")
+  YELLOW = raw("<span style='color:yellow;'>")
